b8647665630ba652bb055a93ae067052dbc5b0df
dilbaay23 <dilbaay@gmail.com>
2020-12-10 00:21:03 +0100

    Permission_Based_Authentication

--- src/main/java/com/moon/springsecuritytutorial2/security/ApplicationSecurityConfig.java
+++ src/main/java/com/moon/springsecuritytutorial2/security/ApplicationSecurityConfig.java
@@ -7,6 +7,8 @@ package com.moon.springsecuritytutorial2.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,9 +17,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import static com.moon.springsecuritytutorial2.security.ApplicationUserPermission.*;
+import static com.moon.springsecuritytutorial2.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)  // if we use annotations with method for permission, we should add this annotation and set prePostEnable=true
 public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -33,9 +38,12 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","index","/css/*", "/js/*")    //we add this paths into white list
-                .permitAll()
-                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+                .antMatchers("/","index","/css/*", "/js/*") .permitAll()    //we add this paths into white list
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())  // we used annotation in controller class with the methods
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())  // this order is very important that it can change all permissions:/
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -48,19 +56,22 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter {
        UserDetails moonUser=  User.builder()
                 .username("moonkoc")
                 .password(passwordEncoder.encode("password"))
-                .roles(ApplicationUserRole.STUDENT.name())  //ROLE_STUDENT
+  //              .roles(ApplicationUserRole.STUDENT.name())  //ROLE_STUDENT
+               .authorities(STUDENT.getGrantedAuthorities())
                .build();
 
         UserDetails luaUser=  User.builder()
                 .username("lua")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ApplicationUserRole.ADMIN.name())  //ROLE_ADMIN
+ //               .roles(ApplicationUserRole.ADMIN.name())  //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails bahaUser=  User.builder()
                 .username("baha")
                 .password(passwordEncoder.encode("password111"))
-                .roles(ApplicationUserRole.ADMINTRAINEE.name())  //ROLE_ADMINTRAINEE
+  //              .roles(ApplicationUserRole.ADMINTRAINEE.name())  //ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
        return new InMemoryUserDetailsManager(moonUser,luaUser,bahaUser);    //this is a class (InMemoryUserDetailsManager) which implements UserDetailsService
--- src/main/java/com/moon/springsecuritytutorial2/security/ApplicationUserRole.java
+++ src/main/java/com/moon/springsecuritytutorial2/security/ApplicationUserRole.java
@@ -1,9 +1,13 @@
 package com.moon.springsecuritytutorial2.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.moon.springsecuritytutorial2.security.ApplicationUserPermission.*;
 
@@ -24,5 +28,14 @@ public enum ApplicationUserRole {
 
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission ->new SimpleGrantedAuthority(permission.getPermission()))
+                        .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+
     }
 }
